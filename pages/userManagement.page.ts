ff13efbdaa243d83492dc51ed8763e657535f1da
Kristi Ingco <kristiana.d.ingco@gmail.com>
2024-07-05 19:02:29 -0700

    Add test for filtering users by user role

--- pages/userManagement.page.ts
+++ pages/userManagement.page.ts
@@ -4,18 +4,34 @@ import { BasePage } from "./base.page";
 export class UserManagementPage extends BasePage {
     readonly addUserButton: Locator;
     readonly userRecordsList: Locator;
+    readonly userRoleSelect: Locator;
+    readonly activeListBox: Locator;
+    readonly searchButton: Locator;
 
     constructor(page: Page, url: string) {
         super(page, url);
         this.addUserButton = page.getByRole("button", { name: "Add" });
         this.userRecordsList = page.getByRole("table");
+        this.userRoleSelect = page.locator(".oxd-select-wrapper").nth(0);
+        this.activeListBox = page.getByRole("listbox");
+        this.searchButton = page.getByRole("button", { name: "Search" });
     }
 
     async clickAddUserButton() {
         await this.addUserButton.click();
     }
 
-    async assertInRecords(username: string) {
-        await expect(this.userRecordsList).toContainText(username);
+    async filterUsersByRole(userRole: string) {
+        await this.userRoleSelect.click();
+        await this.activeListBox.getByText(userRole).click();
+        await this.searchButton.click();
+    }
+
+    async assertInRecords(text: string) {
+        await expect(this.userRecordsList).toContainText(text);
+    }
+
+    async assertNotInRecords(text: string) {
+        await expect(this.userRecordsList).not.toContainText(text);
     }
 }
---
+++ tests/filterUsers.spec.ts
@@ -0,0 +1,36 @@
+import { test } from "@playwright/test";
+import { LoginPage } from "../pages/login.page";
+import { SidePanel } from "../components/sidePanel.component";
+import { UserManagementPage } from "../pages/userManagement.page";
+import { Navbar } from "../components/navbar.component";
+import constants from "../constants";
+
+test.describe("Filter users", () => {
+    let loginPage: LoginPage;
+    let sidePanel: SidePanel;
+    let userManagementPage: UserManagementPage;
+    const { VALID_USERNAME, VALID_PASSWORD } = constants;
+
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page, "/web/index.php/auth/login");
+        await loginPage.visit();
+        await loginPage.login(VALID_USERNAME, VALID_PASSWORD);
+        sidePanel = new SidePanel(page);
+        await sidePanel.clickOnAction("Admin");
+        userManagementPage = new UserManagementPage(
+            page,
+            "/web/index.php/admin/viewSystemUsers"
+        );
+    });
+
+    test("User Management page loads", async ({ page }) => {
+        const navbar = new Navbar(page);
+        await navbar.assertTextInNavbar("User Management");
+    });
+
+    test("Filter users by User Role", async () => {
+        await userManagementPage.filterUsersByRole("ESS");
+        await userManagementPage.assertInRecords("ESS");
+        await userManagementPage.assertNotInRecords("Admin");
+    });
+});
