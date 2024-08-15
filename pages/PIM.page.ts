92b6c69722294c32a1fe148c5549de2dfd99a985
Kristi Ingco <kristiana.d.ingco@gmail.com>
2024-08-15 04:26:55 -0700

    Add assertion method for finding employee in record list

--- pages/PIM.page.ts
+++ pages/PIM.page.ts
@@ -1,15 +1,21 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 import { BasePage } from "./base.page";
 
 export class PIMPage extends BasePage {
     readonly addEmployeeButton: Locator;
+    readonly employeeList: Locator;
 
     constructor(page: Page, url: string) {
         super(page, url);
         this.addEmployeeButton = page.getByText("Add Employee");
+        this.employeeList = page.locator(".oxd-table-body");
     }
 
     async clickAddEmployeeButton() {
         await this.addEmployeeButton.click();
+    }
+
+    async assertEmployeeInEmployeeList(id: string) {
+        await expect(this.employeeList).toContainText(id);
     }
 }
