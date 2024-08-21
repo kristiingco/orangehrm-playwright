e2bfb75713acfb7d728265118c85655dd1ee3965
Kristi Ingco <kristiana.d.ingco@gmail.com>
2024-08-21 11:46:54 -0700

    Add locator attribute for searching by employee Id

--- pages/PIM.page.ts
+++ pages/PIM.page.ts
@@ -3,11 +3,17 @@ import { BasePage } from "./base.page";
 
 export class PIMPage extends BasePage {
     readonly addEmployeeButton: Locator;
+    readonly employeeIdInput: Locator;
     readonly employeeList: Locator;
 
     constructor(page: Page, url: string) {
         super(page, url);
         this.addEmployeeButton = page.getByText("Add Employee");
+        this.employeeIdInput = page
+            .locator(
+                "div[class='oxd-input-group oxd-input-field-bottom-space'] input"
+            )
+            .nth(1);
         this.employeeList = page.locator(".oxd-table-body");
     }
 
